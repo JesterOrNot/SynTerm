702d55997981707d3fe407a8dd81bdd3a972b7ba
Sean Hellum <seanhellum45@gmail.com>
2019-12-25 18:32:44 +0000

    make lib.rs importable

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 #![crate_type = "lib"]
 #![crate_name = "synterm"]
-///! This struct defines a highlighting pair
+/// This struct defines a highlighting pair
 pub struct HighlightingPair<'t> {
     item: &'t str,
     color: &'t str,
 }
 
-///! This will split the input into tokens to parse later
+/// This will split the input into tokens to parse later
 pub fn split_tokens(current_line: &str) -> Vec<&str> {
     return current_line.split(" ").collect();
 }
 
-///! Add tokens enumeration
+/// Add tokens enumeration
 pub enum Tokens {
     Number,
     Operator,
     Comment,
     Keyword,
-}
 }
