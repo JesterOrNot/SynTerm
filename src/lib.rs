9bb449913ef4289ab2ef2ebf7ec0cfae3e625831
Sean Hellum <seanhellum45@gmail.com>
2019-12-25 17:28:55 +0000

    add doc comments
    Signed-off-by: Sean Hellum <seanhellum45@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+//! This struct defines a highlighting pair
 struct HighlightingPair<'T> {
     item: &'T str,
     color: &'T str,
 }
 
+///! This will split the input into tokens to parse later
 fn split_tokens(current_line: &str) -> Vec<&str> {
     return current_line.split(" ").collect();
 }
+
