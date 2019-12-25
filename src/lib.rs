5350fbd14048de6338452d4a2ab6ee884fd4803f
Sean Hellum <seanhellum45@gmail.com>
2019-12-25 18:32:25 +0000

    make a crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
-//! This struct defines a highlighting pair
-struct HighlightingPair<'T> {
-    item: &'T str,
-    color: &'T str,
+#![crate_type = "lib"]
+#![crate_name = "synterm"]
+///! This struct defines a highlighting pair
+pub struct HighlightingPair<'t> {
+    item: &'t str,
+    color: &'t str,
 }
 
 ///! This will split the input into tokens to parse later
-fn split_tokens(current_line: &str) -> Vec<&str> {
+pub fn split_tokens(current_line: &str) -> Vec<&str> {
     return current_line.split(" ").collect();
 }
 
 ///! Add tokens enumeration
-enum Tokens {
+pub enum Tokens {
     Number,
     Operator,
     Comment,
     Keyword,
+}
 }
