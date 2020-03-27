9e5af3266b76375b61050d8cfc72e04a4094a8be
Sean Hellum <seanhellum45@gmail.com>
2020-03-27 01:03:13 +0000

    add generators

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use crossterm::{
     event::{read, Event, KeyCode, KeyModifiers},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
+use logos::{Lexer, Logos};
 use std::collections::HashMap;
 use std::env::{current_dir, var};
 use std::fs::{create_dir_all, File, OpenOptions};
@@ -23,6 +24,40 @@ fn lines_from_file<T: AsRef<Path>>(filename: T) -> impl Iterator<Item = String> 
     };
     let buf = BufReader::new(file);
     buf.lines().map(|l| l.expect("Could not parse line"))
+}
+
+macro_rules! gen_lexer {
+    ($enumName:ident, $(($token:ident,$target:literal)), *) => {
+        #[derive(Logos, Debug, Clone, PartialEq, Eq)]
+        enum $enumName {
+            #[end]
+            End,
+            #[error]
+            Error,
+            #[token = " "]
+            Whitespace,
+            $(
+                #[token = $target]
+                $token,
+            )*
+        }
+    };
+}
+
+macro_rules! gen_parse {
+    ($enumName:ident, $funcName:ident, $(($token:ident, $ansi:literal)), *) => {
+        fn $funcName(mut tokens: Lexer<$enumName, &str>) {
+            while tokens.token != $enumName::End {
+                match tokens.token {
+                    $(
+                        $enumName::$token => print!("\x1b[{}m{}\x1b[m", $ansi, tokens.slice()),
+                    )*
+                    _ => print!("{}", tokens.slice())
+                }
+                tokens.advance();
+            }
+        }
+    };
 }
 
 #[allow(dead_code)]
